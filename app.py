3b726a6be5665d770e606228dd7bac1edf722770
EmberDes <shashwatsinghdit@gmail.com>
2024-09-24 12:04:03 +0000

    day-2

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloworld():
-  return "Hello Jovian"
+  return render_template('home.html')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
